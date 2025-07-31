0bf0572b3dd0b3d8c178c89f04fc3d3a6ea0ce63
juguarnieri <juliaguarnieri04@gmail.com>
2025-07-31 09:22:02 -0300

    refactor: details

--- pages/DetailScreen.js
+++ pages/DetailScreen.js
@@ -19,27 +19,4 @@ const styles = StyleSheet.create({
   text: {
     fontSize: 18
   }
-});import React from 'react';
-import { View, Text, Image, StyleSheet } from 'react-native';
-import Card from "../components/CardDetails"
-
-export default function JuliaDetail() {
-  return (
-    <View style={styles.container}>
-      <Card
-    title="Júlia Guarnieri"
-    image={require("../assets/fotoJulia.png")}
-    description="Olá meu nome é Júlia Guarnieri, sou aluna do Senai e do Sesi Valinhos, atualmente cursando o ensino médio e o curso de desenvolvimento de sistema, tenho 17 anos, moro em valinhos, e minha matéria preferida é matemática"
-      />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create ({
-    container: {
-        padding: 20,
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-    }
-})
+});
